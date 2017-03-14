be1a4449827aeca752afac7ec2dc69798bd942ab
Rickie Cashwell <rjc6056@gmail.com>
2017-03-14 16:40:30 -0400

    changes

--- src/main/java/com/theironyard/ArrayKatas.java
+++ src/main/java/com/theironyard/ArrayKatas.java
@@ -1,5 +1,8 @@
 package com.theironyard;
 
+import java.time.LocalDate;
+import java.util.Random;
+
 /**
  * This is another kata exercise. Follow the instructions below and implement
  * the methods as described.
@@ -18,7 +21,13 @@ public class ArrayKatas {
      * @return an array of five random ints
      */
     // todo: create fiveRandomInts() static method
+    public static int[] fiveRandomInts(){
+        Random ran = new Random();
+        int[] randomInts = new int[]{ran.nextInt(), ran.nextInt(), ran.nextInt(), ran.nextInt(), ran.nextInt()};
 
+        return randomInts;
+
+    }
     /**
      * Create a static method named threeArbitraryDates() that returns an array
      * of three LocalDate objects. Do not use array literal syntax in this
@@ -27,7 +36,10 @@ public class ArrayKatas {
      * @return an array of three LocalDate objects
      */
     // todo: create threeArbitraryDates() method
-
+    public static LocalDate[] threeArbitraryDates(){
+        LocalDate[] objects = new LocalDate[3];
+        return objects;
+    }
     /**
      * Create a static method named nullsToArbitraryStrings() that accepts an
      * array of String objects. This array will always have five elements. The
@@ -47,11 +59,36 @@ public class ArrayKatas {
      *
      * Note that you do not need to set the size of the array in the argument.
      *
-     * @param source an array of five Strings, some of which are null.
+     * //@param source an array of five Strings, some of which are null.
      * @return an array of five string where null values have been replaced with arbitrary strings
      */
     // todo: create nullsToArbitraryStrings() method
+    public static String[] nullsToArbitraryStrings(String[] source){
 
+
+       if(source[0] == null){
+            source[0] = "yes";
+        }
+       if(source[1] == null){
+            source[1] ="I";
+        }
+
+        if(source[2] == null){
+            source[2] ="car";
+        }
+        if(source[3] == null){
+            source[3] ="sit";
+        }
+
+        if(source[4] == null){
+            source[4] ="cat";
+        }
+
+
+        return source;
+
+
+    }
     /**
      * Create a static method named arrayIndexes() that accepts a single integer
      * argument, n. The method should create a new array of integers of this
@@ -64,5 +101,16 @@ public class ArrayKatas {
      * @return An array of ints, starting at 0 and continuing to n-1
      */
     // todo: create arrayIndexes() method
+    public static int[] arrayIndexes(int n){
 
+        int[] array = new int[n];
+        int counter =0;
+
+        for(int i = 0; i < n; i++){
+            array[i] += counter;
+            counter++;
+        }
+
+        return array;
+    }
 }
---
+++ src/main/java/com/theironyard/Main.java
@@ -0,0 +1,14 @@
+package com.theironyard;
+
+/**
+ * Created by rickiecashwell on 3/14/17.
+ */
+public class Main {
+    public static void main(String[] args) {
+        ArrayKatas katas = new ArrayKatas();
+
+
+        System.out.println(katas.arrayIndexes(5));
+
+    }
+}
